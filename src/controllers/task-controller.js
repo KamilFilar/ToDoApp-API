c801ee231b1f4d67761eca8ab4985ff234d5d283
KamilFilar <49237105+KamilFilar@users.noreply.github.com>
2022-03-22 12:17:18 +0100

    Updated method for removing single task

--- src/controllers/task-controller.js
+++ src/controllers/task-controller.js
@@ -63,6 +63,7 @@ export default {
   async removeAllTasks(req, res, next) {
     const queryREMOVE = "DELETE FROM tasks";
 
+    // Open connect with db
     pool.getConnection( (err, connection) => {
       if (err) throw err;
       else {
@@ -77,16 +78,36 @@ export default {
             return res.status(400).send(err);
           }
         });
-        connection.release();
+        connection.release(); // end connection
       }
     });
   },
 
   // Remove existing task from db
   async removeTask(req, res, next) {
-    return res.status(200).send({
-      msg: "Remove task work!",
-    });
+    const TASK_ID = req.body.id;
+    const TASK_NAME = req.body.name;
+
+    const queryREMOVE = "DELETE FROM tasks WHERE `id` = ? AND `name` = ?";
+    
+    // Open connect with db
+    pool.getConnection( (err, connection) => {
+      if (err) throw err;
+      else {
+        connection.query(queryREMOVE, [TASK_ID, TASK_NAME], (err) => {
+          if (!err) {
+            return res.status(200).send({
+              msg: `Task: "${TASK_NAME}" has been removed successful!`
+            })
+          }
+          else {
+            console.log(err);
+            return res.status(400).send(err);
+          }
+        })
+      }
+      connection.release(); // end connection
+    })
   },
 
   // Update concrete task in db
